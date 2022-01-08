d8ad8b86a6622bc75ffe1b3480348ab4352a9e57
POI <jinroup@gmail.com>
2022-01-08 18:31:21 +0900

    プロジェクト作成

---
+++ munou-chozai/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <img src="./assets/title.png" height="400px">
+    コミュ障、精神疾患、努力が報われない人など、
+    どんな人でも肯定し、受け入れます！
+    AIやコロナに仕事を奪われ、世間からも沙汰され、
+    将来に不安を抱いている方々の心の拠り所になるよう、
+    サイト運営頑張ります。
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+
+</style>
